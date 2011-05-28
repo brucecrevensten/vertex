b0f131dab2eb93cfc0ec44ad710eea42a60f3e0d
bcrevensten <bcrevensten@bac3649f-82c9-4cc2-977b-7c85098c3426>
2011-05-28 05:30:04 +0000

    [US224] initial commit of working code that fetches a result set and displays it through a DataTable

--- root/static/js/Search.js
+++ root/static/js/Search.js
@@ -17,6 +17,18 @@ var SearchResults = Backbone.Collection.extend(
     getQueryString: function() {
       return this.url + JSON.stringify(this.searchParameters.toJSON());
     },
+    parseObjectsToArrays: function(d, c) {
+      var a = [];
+      for ( var i=0, iLen=d.length; i < iLen; i++ ) {
+        var inner = [];
+        for ( var j=0, jLen=c.length; j < jLen; j++ ) {
+          inner.push( d[i][c[j]] );
+        }
+        a.push( inner);
+      }
+      console.log(a);
+      return a;
+    },
     fetchSearchResults: function(sp) {
       this.setParameters(sp);
 
@@ -30,7 +42,24 @@ var SearchResults = Backbone.Collection.extend(
         success: function(data, textStatus, jqXHR) {
           this.data = data;
           this.refresh( this.data.results.rows );
-          return this;
+          
+          $('#searchResults').dataTable(
+            {
+            "bJQueryUI": true,
+            "aaData": this.parseObjectsToArrays(data.results.rows, ["GRANULENAME","PROCESSINGTYPE","PLATFORM","ORBIT","FRAMENUMBER","ACQUISITIONDATE","CENTERLAT","CENTERLON"]),
+            "aoColumns": [
+              { "sTitle": "Granule Name" },
+              { "sTitle": "Processing" },
+              { "sTitle": "Platform" },
+              { "sTitle": "Orbit" },
+              { "sTitle": "Frame" },
+              { "sTitle": "Acquisiton Date" },
+              { "sTitle": "Center Lat" },
+              { "sTitle": "Center Lon" }
+            ]
+            }
+            );
+
         },
         error: function(jqXHR, textStatus, errorThrown) {
           //todo: fix this to be meaningful
