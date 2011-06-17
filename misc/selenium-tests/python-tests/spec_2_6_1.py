18fd729ae2ee7bc9114753e3e517964e65697c9e
rwilson <rwilson@bac3649f-82c9-4cc2-977b-7c85098c3426>
2011-06-17 04:17:25 +0000

    [US224] added python tests for selenium

---
+++ misc/selenium-tests/python-tests/spec_2_6_1.py
@@ -0,0 +1,32 @@
+from selenium import selenium
+import unittest, time, re
+
+class spec_2_6_1(unittest.TestCase):
+    def setUp(self):
+        self.verificationErrors = []
+        self.selenium = selenium("localhost", 4444, "*chrome", "http://testapi.daac.asf.alaska.edu/portal")
+        self.selenium.start()
+    
+    def test_spec_2_6_1(self):
+        sel = self.selenium
+        sel.open("/portal")
+        sel.click("link=Date")
+        sel.click("filter_start")
+        sel.click("link=4")
+        try: self.assertEqual("2010-12-04", sel.get_value("filter_start"))
+        except AssertionError, e: self.verificationErrors.append(str(e))
+        for i in range(60):
+            try:
+                if sel.is_text_present("26056"): break
+            except: pass
+            time.sleep(1)
+        else: self.fail("time out")
+        try: self.assertEqual("26056", sel.get_text("css=td:nth(3)"))
+        except AssertionError, e: self.verificationErrors.append(str(e))
+    
+    def tearDown(self):
+        self.selenium.stop()
+        self.assertEqual([], self.verificationErrors)
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ misc/selenium-tests/python-tests/spec_3.py
@@ -0,0 +1,29 @@
+from selenium import selenium
+import unittest, time, re
+
+class spec_3(unittest.TestCase):
+    def setUp(self):
+        self.verificationErrors = []
+        self.selenium = selenium("localhost", 4444, "*chrome", "http://testapi.daac.asf.alaska.edu/portal")
+        self.selenium.start()
+    
+    def test_spec_3(self):
+        sel = self.selenium
+        sel.open("/portal")
+        for i in range(60):
+            try:
+                if sel.is_text_present("26056"): break
+            except: pass
+            time.sleep(1)
+        else: self.fail("time out")
+        try: self.failUnless(sel.is_element_present("//*[@id=\"results_wrapper\"]"))
+        except AssertionError, e: self.verificationErrors.append(str(e))
+        try: self.assertEqual("dataTables_wrapper", sel.get_attribute("//*[@id=\"results_wrapper\"]@class"))
+        except AssertionError, e: self.verificationErrors.append(str(e))
+    
+    def tearDown(self):
+        self.selenium.stop()
+        self.assertEqual([], self.verificationErrors)
+
+if __name__ == "__main__":
+    unittest.main()
