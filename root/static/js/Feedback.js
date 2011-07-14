f718c17a92c147ecb5943d22d5cbaf0048b64d89
rwilson <rwilson@bac3649f-82c9-4cc2-977b-7c85098c3426>
2011-07-14 23:42:23 +0000

    [US222] added initial Feedback.js sketch and feedback url to the asfconfig

--- root/static/js/Feedback.js
+++ root/static/js/Feedback.js
@@ -1,22 +1,60 @@
 var Feedback = Backbone.Model.extend({
 	url: AsfDataportalConfig.feedbackUrl,	
+	validate: function(attrs){
+		
+	},
+	submit_feedback: function(attrs) {		
+		$.ajax({
+			type: "POST",
+			url: AsfDataportalConfig.feedbackUrl,
+			data: { userid: this.get('feedback_comment')},
+			dataType: "json",
+			context: this,
+			success: function(data, textStatus, jqXHR) {
+				this.widgetRenderer = this.getWidgetRenderer();
+			},
+			error: function(error) {
+				console.log(error);
+			}
+		});
+	},
 
 });
 
 var FeedbackForm = Backbone.View.extend({
+	initialize: function() {
+		_.bindAll(this, "render");
+	},
 	render: function() {
-	$(this.el).html(
-        _.template('\
-<div id="feedback">\
-<p> the form would go here</p>
-<a class="feeback_submit" href="<%= URL %>">Submit</a>\
-</div>\
-        ', this.model.toJSON())
-      );
-      $(this.el).find('a').button({
-      	text: 'Download'
-      });
+		$(this.el).html(
+	        _.template('\
+			<div id="feedback">\
+			<p> the form would go here</p>\
+			<form action="<%= URL %>" method="post">
+	  			<input class="required" type="textarea" cols="60" rows="8" name="feedback_comment" /><br />
+			</form>
+			<button id="feedback_button">Submit Feedback</button>
+			</div>\
+	    	', this.model.toJSON())
+	    );
+		
+		
+		$(this.el).dialog({
+	      width: 700,
+	      modal: true,
+	      draggable: false,
+	      resizable: false,
+	      title: "Feedback Form",
+	      position: "center",
+	      buttons: {
+			"Cancel" : function() {
+				$( this).dialog('close');
+			},
+			"Feedback":  function() {
+				this.model.set( $(this.el).serializeArray() );
+				this.model.subnit_feedback();
+	    });
 
-      return this;
+	    return this;
 	}
 });
