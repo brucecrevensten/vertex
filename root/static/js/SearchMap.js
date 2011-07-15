1967dd8faf991685afd0f9871ac53e2bf9aa1cbd
rwilson <rwilson@bac3649f-82c9-4cc2-977b-7c85098c3426>
2011-07-15 17:52:34 +0000

    [US222] adjusted initial gMap to be centered on alaska and zoomed out

--- root/static/js/SearchMap.js
+++ root/static/js/SearchMap.js
@@ -2,11 +2,14 @@ var searchMap;
 
 function initMap(mapContainer) {
   if(window.searchMap) return;
+  
+  var initLatLng = new google.maps.LatLng(65,-150);
 
   window.searchMap = new google.maps.Map(document.getElementById('searchMap'),
     {
+      center: initLatLng,
       mapTypeId: google.maps.MapTypeId.TERRAIN,
-      scrollwheel: false
+      zoom: 8
     });
 }
 
