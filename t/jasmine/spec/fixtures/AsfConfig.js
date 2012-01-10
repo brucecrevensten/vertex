ebbcbc580ce7f0828fa2ca655d0afe7598f4548f
Davin Simmons <dasimmons@alaska.edu>
2012-01-10 19:04:58 +0000

    Updated so that the data structure created in teh AsfConfig.js file is now a fixture for the EMS Page Tag tests rather then hard coding those values into the SpecRunnerPageTag.html document.

---
+++ t/jasmine/spec/fixtures/AsfConfig.js
@@ -0,0 +1,7 @@
+var AsfDataportalConfig={
+  apiUrl:"http://localhost/services/search/json",
+  authUrl:"http://localhost/services/authentication",
+  logoutUrl: "http://localhost/services/destroy_session",
+  feedbackUrl: "http://localhost/services/feedback"
+};
+var AsfConfig = { debug: false };
