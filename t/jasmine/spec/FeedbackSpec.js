376eb9d8ca4b2444d53074ddbdc0aa5080d3964e
Rayjan Wilson <rayjan.wilson@gmail.com>
2012-01-31 00:53:48 -0900

    another basic test added. need to figure out how to test the validation function and view

--- t/jasmine/spec/FeedbackSpec.js
+++ t/jasmine/spec/FeedbackSpec.js
@@ -13,17 +13,16 @@ describe("Feedback.js", function(){
       expect(fb.get('email')).toBe('testuser@somewhere.com');
     });
 
-    it('Provides the correct url', function(){
-      expect(true).toEqual(false);
+    it('Provides the correct url for the model', function(){
+      var fb = new Feedback();
+      expect(fb.get('url')).toBe(AsfDataportalConfig.feedbackUrl);
     });
 
     it('Contains validation rules, and will trigger an error event on failed validation.', function(){
+      // need to figure out how to test this
       expect(true).toEqual(false);
     });
 
-    it('Provides a correct url for the model', function(){
-      expect(true).toEqual(false);
-    });
   });
 
 });
