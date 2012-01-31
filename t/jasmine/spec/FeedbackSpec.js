dd83f645d2f83d73cb1ca9bf1d78d1ae547cd0ff
Rayjan Wilson <rayjan.wilson@gmail.com>
2012-01-31 01:11:44 -0900

    adjusted the url test to look for a property rather than an attribute

--- t/jasmine/spec/FeedbackSpec.js
+++ t/jasmine/spec/FeedbackSpec.js
@@ -2,20 +2,22 @@ describe("Feedback.js", function(){
 
   describe("Feedback: Backbone.Model", function(){
     it('Can be created with default values for its attributes.', function() {
-      this.fb = new Feedback();
-      expect(this.fb.get("name")).toBe('unknown');
-      expect(this.fb.get("email")).toBe('unknown');
+      var fb = new Feedback();
+      console.log('feedback');
+      console.log(fb);
+      expect(fb.get("name")).toBe('unknown');
+      expect(fb.get("email")).toBe('unknown');
     });
 
     it('Will set passed attributes on the model instance when created.', function() {
-      this.fb = new Feedback({'name': 'Test User', 'email': 'testuser@somewhere.com'});
-      expect(this.fb.get('name')).toBe('Test User');
-      expect(this.fb.get('email')).toBe('testuser@somewhere.com');
+      var fb = new Feedback({'name': 'Test User', 'email': 'testuser@somewhere.com'});
+      expect(fb.get('name')).toBe('Test User');
+      expect(fb.get('email')).toBe('testuser@somewhere.com');
     });
 
     it('Provides the correct url for the model', function(){
-      this.fb = new Feedback();
-      expect(this.fb.get('url')).toBe(AsfDataportalConfig.feedbackUrl);
+      var fb = new Feedback();
+      expect(fb.url).toBe(AsfDataportalConfig.feedbackUrl);
     });
 
     it('Contains validation rules, and will trigger an error event on failed validation.', function(){
